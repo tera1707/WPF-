a8076cc48125086f4d2c3ab8b95efffc38adcd93
tera1707 <Masa1017>
2018-12-12 21:32:14 +0900

    サンプルUP

---
+++ 001_INotifyPropertyChanged/WpfApp1/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class ViewModel : BindingBase
+    {
+        public int MyValue
+        {
+            get
+            {
+                return _myValue;
+            }
+            set
+            {
+                if (_myValue != value)
+                {
+                    _myValue = value;
+                    OnPropertyChanged(nameof(MyValue));
+                }
+            }
+        }
+        private int _myValue = 123;
+
+
+
+    }
+}
