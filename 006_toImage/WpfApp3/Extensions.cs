0c56f2a520a0118d5e8afdddb436a9bebd1b05c3
tera1707 <Masa1017>
2018-12-18 22:00:42 +0900

    sample

---
+++ 006_toImage/WpfApp3/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace WpfApp3
+{
+    // Canvas クラスの拡張メソッドとして実装する
+    public static class CanvasExtensions
+    {
+        // Canvas を画像ファイルとして保存する。
+        public static void toImage(this Canvas canvas, string path, BitmapEncoder encoder = null)
+        {
+            // レイアウトを再計算させる
+            var size = new Size(canvas.Width, canvas.Height);
+            canvas.Measure(size);
+            canvas.Arrange(new Rect(size));
+
+            // VisualObjectをBitmapに変換する
+            var renderBitmap = new RenderTargetBitmap((int)size.Width,       // 画像の幅
+                                                      (int)size.Height,      // 画像の高さ
+                                                      96.0d,                 // 横96.0DPI
+                                                      96.0d,                 // 縦96.0DPI
+                                                      PixelFormats.Pbgra32); // 32bit(RGBA各8bit)
+            renderBitmap.Render(canvas);
+
+            // 出力用の FileStream を作成する
+            using (var os = new FileStream(path, FileMode.Create))
+            {
+                // 変換したBitmapをエンコードしてFileStreamに保存する。
+                // BitmapEncoder が指定されなかった場合は、PNG形式とする。
+                encoder = encoder ?? new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(renderBitmap));
+                encoder.Save(os);
+            }
+        }
+    }
+
+}
