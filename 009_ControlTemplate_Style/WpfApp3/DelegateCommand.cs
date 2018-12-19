f87f61bfa8c8bb1505e583ede4728adc6b29abf0
tera1707 <Masa1017>
2018-12-19 22:55:04 +0900

    ControlTemplateとStyleでボタンをカスタム、そこにIcommandのサンプル

---
+++ 009_ControlTemplate_Style/WpfApp3/DelegateCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp3
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        System.Action<T> execute;
+        System.Func<bool> canExecute;
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+
+        public event System.EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            execute((T)parameter);
+        }
+
+        public DelegateCommand(System.Action<T> execute)
+        {
+            this.execute = execute;
+        }
+
+        public DelegateCommand(System.Action<T> execute, System.Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+    }
+}
---
+++ 009_ControlTemplate_Style/WpfApp3/ViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp3
+{
+    class ViewModel
+    {
+        public DelegateCommand<string> MyCommand { get; private set; }
+
+        public ViewModel()
+        {
+            MyCommand = new DelegateCommand<string>(
+                (param) =>
+                {
+                    // ボタンをおしたときに行う処理
+                    MessageBox.Show("ボタン：" + param);
+                },
+                () =>
+                {
+                    // ボタンが押せるかどうかを決める処理(trueが「押せる」)
+                    return true;
+                });
+        }
+    }
+}
