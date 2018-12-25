571dcfa45038522a29b25404a82911197c9e6dbf
tera1707 <Masa1017>
2018-12-25 23:09:24 +0900

    ドラッグして移動できる、テキストを入力するためのPad実験

---
+++ 010_DraggableTextPad/WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+// https://araramistudio.jimdo.com/2016/12/06/wpf%E3%81%A7%E3%83%89%E3%83%A9%E3%83%83%E3%82%B0%E3%81%A7%E3%81%8D%E3%82%8B%E3%82%B3%E3%83%B3%E3%83%88%E3%83%AD%E3%83%BC%E3%83%AB%E3%82%92%E4%BD%9C%E3%82%8B/
+namespace WpfApp5
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Thumb_DragStarted(object sender, System.Windows.Controls.Primitives.DragStartedEventArgs e)
+        {
+        }
+
+        private void Thumb_DragCompleted(object sender, System.Windows.Controls.Primitives.DragCompletedEventArgs e)
+        {
+        }
+
+        private void Thumb_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            var thumb = sender as Thumb;
+            if (null != thumb)
+            {
+                var x = Canvas.GetLeft(thumb) + e.HorizontalChange;
+                var y = Canvas.GetTop(thumb) + e.VerticalChange;
+
+                var canvas = thumb.Parent as Canvas;
+                if (null != canvas)
+                {
+                    x = Math.Max(x, 0);
+                    y = Math.Max(y, 0);
+                    x = Math.Min(x, canvas.ActualWidth - thumb.ActualWidth);
+                    y = Math.Min(y, canvas.ActualHeight - thumb.ActualHeight);
+                }
+
+                Canvas.SetLeft(thumb, x);
+                Canvas.SetTop(thumb, y);
+            }
+        }
+    }
+}
