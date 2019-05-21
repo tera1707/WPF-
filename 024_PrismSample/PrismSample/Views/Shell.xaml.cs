fe8771f7b4acffaceabd0d15a2dfd986d75edebf
tera1707 <Masa1017>
2019-05-21 20:43:18 +0900

    Shell.xaml.csに、初期画面表示機能を追加

--- 024_PrismSample/PrismSample/Views/Shell.xaml.cs
+++ 024_PrismSample/PrismSample/Views/Shell.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Practices.Unity;
+using Prism.Regions;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace PrismSample.Views
 {
@@ -19,9 +10,16 @@ namespace PrismSample.Views
     /// </summary>
     public partial class Shell : Window
     {
+        [Dependency]
+        public IRegionManager RegionManager { get; set; }
         public Shell()
         {
             InitializeComponent();
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            this.RegionManager.RequestNavigate("MainRegion", nameof(UserControl1));
         }
     }
 }
