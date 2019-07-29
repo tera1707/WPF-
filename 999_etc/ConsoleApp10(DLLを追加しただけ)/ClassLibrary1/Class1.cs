24d6a6ecbdce4c3ed6f9a59bebb87d85451e313f
tera1707 <Masa1017>
2019-07-29 22:06:37 +0900

    dll追加しただけ

---
+++ 999_etc/ConsoleApp10(DLLを追加しただけ)/ClassLibrary1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static int Add(int a, int b)
+        {
+            return Class2.Add2(a, b);
+        }
+    }
+}
---
+++ 999_etc/ConsoleApp10(DLLを追加しただけ)/ClassLibrary2/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Class2
+    {
+        public static int Add2(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
