556f47b87ffb7174a0efa4253fa450ed4a1d3616
tera1707 <Masa1017>
2019-08-21 00:05:01 +0900

    初回コミット

---
+++ 030_UwpPackage/ConsoleApp12/Program.cs
@@ -0,0 +1,27 @@
+﻿using ClassLibrary1;
+using System;
+using System.Diagnostics;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = string.Empty;
+
+            try
+            {
+                var localDir = Windows.Storage.ApplicationData.Current.LocalFolder;
+                path = localDir.Path;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            }
+
+            Debug.WriteLine("target path is : " + path);
+        }
+    }
+}
