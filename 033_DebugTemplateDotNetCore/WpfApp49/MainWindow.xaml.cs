306d7b5a3efd7986ffff41ee5b1d5df1aded778e
tera1707 <Masa1017>
2020-04-17 10:08:40 +0900

    初回コミット

---
+++ 033_DebugTemplateDotNetCore/WpfApp49/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp49
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+
+        #region LogFramework
+        public ObservableCollection<string> Logs { get; set; } = new ObservableCollection<string>();
+
+        public void AddLog(string log)
+        {
+            DateTime now = DateTime.Now;
+
+            Logs.Insert(0, now.ToString("hh:mm:ss.fff ") + log);
+            OnPropertyChanged(nameof(Logs));
+        }
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Root_Loaded(object sender, RoutedEventArgs e)
+        {
+            AddLog("ログ");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
