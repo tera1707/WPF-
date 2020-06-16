672ebab615dfac5249519361deff0a005a1092e7
tera1707 <Masa1017>
2020-06-16 23:27:04 +0900

    初回コミット

---
+++ 036_BindingWithUserControl/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public double DoubleValue1
+        {
+            get { return _doubleValue1; }
+            set { Console.WriteLine("* MainWindow DoubleValue1 = {0} ", value); _doubleValue1 = value; OnPropertyChanged(nameof(DoubleValue1)); }
+        }
+        private double _doubleValue1 = 0.0;
+
+        public double DoubleValue2
+        {
+            get { return _doubleValue2; }
+            set { Console.WriteLine("* MainWindow DoubleValue2 = {0} ", value); _doubleValue2 = value; OnPropertyChanged(nameof(DoubleValue2)); }
+        }
+        private double _doubleValue2 = 0.0;
+
+        public double DoubleValue3
+        {
+            get { return _doubleValue3; }
+            set { Console.WriteLine("* MainWindow DoubleValue3 = {0} ", value); _doubleValue3 = value; OnPropertyChanged(nameof(DoubleValue3)); }
+        }
+        private double _doubleValue3 = 0.0;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleValue1++;
+            DoubleValue2++;
+            DoubleValue3++;
+        }
+    }
+}
---
+++ 036_BindingWithUserControl/WpfApp1/SimpleUserControl.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class SimpleUserControl : UserControl
+    {
+        //---------------------------------------------------------------------------
+
+        public double MyDoubleValue1
+        {
+            get => (double)GetValue(MyDoubleValue1Property);
+            set { Console.WriteLine(" - UserControl DoubleValue1 = {0} ", value); SetValue(MyDoubleValue1Property, value); }
+        }
+        public static readonly DependencyProperty MyDoubleValue1Property = DependencyProperty.Register(nameof(MyDoubleValue1), typeof(double), typeof(SimpleUserControl));
+
+        //---------------------------------------------------------------------------
+
+        public double MyDoubleValue2
+        {
+            get => (double)GetValue(MyDoubleValue2Property);
+            set { Console.WriteLine(" - UserControl DoubleValue2 = {0} ", value); SetValue(MyDoubleValue2Property, value); }
+        }
+        public static readonly DependencyProperty MyDoubleValue2Property = DependencyProperty.Register(nameof(MyDoubleValue2), typeof(double), typeof(SimpleUserControl));
+
+        //---------------------------------------------------------------------------
+
+        public double MyDoubleValue3
+        {
+            get => (double)GetValue(MyDoubleValue3Property);
+            set { Console.WriteLine(" - UserControl DoubleValue3 = {0} ", value); SetValue(MyDoubleValue3Property, value); }
+        }
+        public static readonly DependencyProperty MyDoubleValue3Property = DependencyProperty.Register(nameof(MyDoubleValue3), typeof(double), typeof(SimpleUserControl));
+
+        //---------------------------------------------------------------------------
+
+        // コンストラクタ
+        public SimpleUserControl()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// UserControl側で数字を＋＋
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyDoubleValue1 += 1.0;
+            MyDoubleValue2 += 1.0;
+            MyDoubleValue3 += 1.0;
+        }
+    }
+}
