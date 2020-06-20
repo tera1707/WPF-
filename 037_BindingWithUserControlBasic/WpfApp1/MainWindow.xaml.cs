571c33eee0c88bbb661797d0d279110569724e36
tera1707 <Masa1017>
2020-06-20 23:51:03 +0900

    初回コミット

---
+++ 037_BindingWithUserControlBasic/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName) => this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public double DoubleValue1
+        {
+            get { return _doubleValue1; }
+            set { Console.WriteLine("* MainWindow DoubleValue1 = {0} ", value); _doubleValue1 = value; OnPropertyChanged(nameof(DoubleValue1)); }
+        }
+        private double _doubleValue1 = 0.0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DoubleValue1++;
+        }
+    }
+}
---
+++ 037_BindingWithUserControlBasic/WpfApp1/SimpleUserControl.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public partial class SimpleUserControl : UserControl
+    {
+        public double MyDoubleValue1
+        {
+            get => (double)GetValue(MyDoubleValue1Property);
+            set { SetValue(MyDoubleValue1Property, value); }
+        }
+        public static readonly DependencyProperty MyDoubleValue1Property = DependencyProperty.Register(nameof(MyDoubleValue1), typeof(double), typeof(SimpleUserControl), new PropertyMetadata(0.0));
+        
+        public SimpleUserControl()
+        {
+            InitializeComponent();
+        }
+private void Button_Click(object sender, RoutedEventArgs e)
+{
+    MyDoubleValue1 += 1.0;
+}
+    }
+}
