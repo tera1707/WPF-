d1324fc1733d37c26ff04d42099ca811c62293d4
tera1707 <Masa1017>
2020-06-24 23:38:05 +0900

    初回コミット

---
+++ 038_DrawLines/WpfApp57/Converter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Converter
+{
+    public class ListPointToPointCollectionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var points = (List<Point>)value;
+            return points != null ? new PointCollection(points) : null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 038_DrawLines/WpfApp57/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp57
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        // ------------------------------------------------
+
+        public List<Point> Points
+        {
+            get { return _points; }
+            set { _points = value; OnPropertyChanged(nameof(Points)); }
+        }
+        private List<Point> _points = new List<Point>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Points.Add(new Point(10, 10));
+            Points.Add(new Point(100, 10));
+            Points.Add(new Point(100, 100));
+            Points.Add(new Point(10, 100));
+            Points.Add(new Point(10, 10));
+            OnPropertyChanged(nameof(Points));
+        }
+    }
+}
