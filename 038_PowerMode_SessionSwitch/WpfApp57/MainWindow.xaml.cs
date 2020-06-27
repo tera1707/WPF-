5c7fd19466ddf918b352731f209109e811f0d601
tera1707 <Masa1017>
2020-06-27 23:56:40 +0900

    初回コミット

--- 038_PowerMode_SessionSwitch/WpfApp57/MainWindow.xaml.cs
+++ 038_PowerMode_SessionSwitch/WpfApp57/MainWindow.xaml.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,25 +16,38 @@ namespace WpfApp57
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        #region INotifyPropertyChanged
         public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged(string name = null)
+        protected void OnPropertyChanged(string propertyName)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+        #endregion
+
+        #region LogFramework
+        public ObservableCollection<string> Logs { get; set; } = new ObservableCollection<string>();
+
+        public void AddLog(string log)
+        {
+            DateTime now = DateTime.Now;
+
+            // リストに表示
+            var l = now.ToString("hh:mm:ss.fff ") + log;
+            Logs.Insert(0, l);
+            OnPropertyChanged(nameof(Logs));
+
+            // ログファイルに書き込む
+            Assembly myAssembly = Assembly.GetEntryAssembly();
+            string path = Path.GetDirectoryName(myAssembly.Location) + @"\testlog.txt";
+            using (var sw = new StreamWriter(path, true))
+            {
+                sw.Write(l + "\r\n");
+            }
+        }
+        #endregion
 
         // ------------------------------------------------
 
-        // サインカーブの縦方向の真ん中座標
-        private double y = 150.0;
-
-        public List<Point> Points
-        {
-            get { return _points; }
-            set { _points = value; OnPropertyChanged(nameof(Points)); }
-        }
-        private List<Point> _points = new List<Point>();
-        
         public MainWindow()
         {
             InitializeComponent();
@@ -45,7 +61,7 @@ namespace WpfApp57
 
         private void SystemEvents_PowerModeChanged(object sender, PowerModeChangedEventArgs e)
         {
-            Debug.WriteLine(e.Mode);
+            AddLog(e.Mode.ToString());
 
             switch (e.Mode)
             {
@@ -61,21 +77,17 @@ namespace WpfApp57
 
         private void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
         {
-            Debug.WriteLine(e.Reason);
+            AddLog(e.Reason.ToString());
 
             switch (e.Reason)
             {
                 case SessionSwitchReason.ConsoleConnect:
-                    Debug.WriteLine("ConsoleConnect");
                     break;
                 case SessionSwitchReason.ConsoleDisconnect:
-                    Debug.WriteLine("ConsoleDisconnect");
                     break;
                 case SessionSwitchReason.RemoteConnect:
-                    Debug.WriteLine("RemoteConnect");
                     break;
                 case SessionSwitchReason.RemoteDisconnect:
-                    Debug.WriteLine("RemoteDisconnect");
                     break;
                 default:
                     break;
@@ -84,19 +96,7 @@ namespace WpfApp57
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var _ = Task.Run(() =>
-            {
-                this.Dispatcher.Invoke(new Action(async () =>
-                {
-                    for (int i = 0; i < 100; i++)
-                    {
-                        Points.Add(new Point((double)i, y + 150.0 * Math.Sin((double)i / (Math.PI))));
-                        OnPropertyChanged(nameof(Points));
-                        await Task.Delay(30);
-                    }
-                }));
-            });
-
+            AddLog("Test");
         }
     }
 }
